7275527d91d889a5cc7db4196631bfa0dee856c9
Mohammad Javad <mohaadjavad.2005@gmail.com>
2025-12-17 21:53:19 +0330

    add categories.ts to the constanst folder

---
+++ client/constants/categories.ts
@@ -0,0 +1,19 @@
+export const expenseCategories = [
+  'Food & Dining',
+  'Transportation',
+  'Housing & Rent',
+  'Utilities',
+  'Healthcare',
+  'Entertainment',
+  'Clothing',
+  'Education',
+  'Other'
+];
+
+export const incomeCategories = [
+  'Salary',
+  'Freelance',
+  'Investments',
+  'Gifts',
+  'Other'
+];
